d2b8b15837000257dd445df77b9a917dbf1d5b88
Oded Fisher <36313926+odedf23232@users.noreply.github.com>
2022-08-25 17:50:07 +0300

    Add files via upload

---
+++ Enums/Constants.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTLProject.Enums
+{
+    class Constants
+    {
+        /// <summary>
+        /// Tarit models names
+        /// </summary>
+        public const string NoQTL = "No QTL";
+        public const string OneQTL = "1 QTL";
+        public const string DominantQTL = "Dominant 1 QTL";
+        public const string TwoLinkedQTL = "2 Linked QTL";
+
+        /// <summary>
+        /// Genetic table header titles
+        /// </summary>
+        public const string ChrNum = "Chr Num.";
+        public const string ChrLen = "Chr Len. (cM)";
+        public const string MarkerPerCM = "Marker per cM";
+       
+        /// <summary>
+        /// Error size titles 
+        /// </summary>
+        public const string PopSize = "Pop Size";
+        public const string MissingData = "Missing Data %";
+        public const string Error = "Error %";
+        public const string ChrAmount = "Chr Amount";
+
+        /// <summary>
+        ///  Trait table column names
+        /// </summary>
+        public const string QtlPos= "QTL pos (cM)";
+        public const string QTLChr="QTL Chr";
+        public const string VarQ="Var Q";
+        public const string Varq="Var q";
+        public const string AvgQ="Avq. Q";
+        public const string Avgq="Avq. q";
+        public const string DominanceEffect = "Dom. Effect";
+
+
+        /// <summary>
+        /// Table columns for input Data table
+        /// </summary>
+        public const string Marker = "Marker";
+        public const string CoorcM = "Coor(cM)";
+        public const string Chr = "Chr";
+        public const string Quality = "Quality";
+        public const string Map = "Map";
+
+        /// <summary>
+        /// Table names
+        /// </summary>
+        public const string GeneticTableRowName = "GeneticTableRow";
+        public const string TraitTableRowName = "TraitTableRow";
+        public const string InputDataTableRowName = "InputDataTableRow";
+        /// <summary>
+        /// Tool tips strings for Input Data View
+        /// </summary>
+        public const string LoadFile = "Loads File";
+        public const string SaveFile = "Saves current Table";
+        public const string CopiesRows = "Copies the selected rows";
+        public const string PasteRows = "Pastes the selected rows";
+        public const string InsertRows = "Inserts rows at the end of the table";
+        public const string RemoveRows = "Removes rows from the end of the table";
+
+        /// <summary>
+        /// Tool tip string for Calculate QTL View
+        /// </summary>
+        public const string OpenGenFile = "Browse for Genotype File";
+        public const string OpenPhenFile = "Browse for Phenotype File";
+
+        /// <summary>
+        /// Tool tip for  all Views
+        /// </summary>
+        public const string GoToNextStage = "Go to next stage";
+        public const string GoToPrevStage = "Go to prevoius stage";
+
+        /// <summary>
+        /// Types of tests to preform 
+        /// </summary>
+        public const string DistributionTest = "Distribution of Trait";
+        public const string SingleMarkerTest = "Single Marker Tests";
+        public const string QTLPosition = "QTL Position";
+        public const string QTLsEffect = "QTLs Effect";
+        public const string TraitDistributionforQTLalleles = "Trait Distribution for QTL alleles";
+        public const string ModelComparison = "Model Comparison";
+        public const string Power = "Power";
+
+
+    }
+
+    class ColorConstants
+    {
+        public static Color tableHeaderColor = Color.FromArgb(199, 243, 252);
+        public static Color backgroundColor = Color.FromArgb(239, 252, 255);
+        public static Color backgroundContrastColor = Color.FromArgb(255, 255, 255);
+        public static Color tableBackgroundColor = Color.FromArgb(242, 242, 242);
+        public static Color toolbarButtonsColor = Color.FromArgb(235, 249, 252);
+        public static Color highliteColor = Color.FromArgb(255, 0, 229);
+    }
+}
---
+++ Enums/Types.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTLProject
+{
+    public static class Types
+    {
+        /// <summary>
+        ///  Complete dominance, Incomplete dominance, and Codominance
+        /// 0 - allele 0 is dominant, 1 - allele 1 is dominant, 2 - codominant
+        /// </summary>
+        public enum LocusDominanceStatus
+        {
+          CompleteDominance=0,
+          IncompleteDominance=1,
+          Codominant=2
+        }
+
+        //0   - Allele 1 is recessive
+        //1   - Allele 1 is dominant
+        //0.5 - Allele 1 is codominant
+        public static class AdditiveEffect_h 
+        {
+            public const double Recessive = 0.0;
+            public const double Codominant = 0.5;
+            public const double Dominant = 1.0;
+
+        }
+
+        /// <summary>
+        /// Gender : 0 - Female, 1 - Male
+        /// </summary>
+        public enum Gender
+        {
+            Female=0,
+            Male=1
+        }
+        /// <summary>
+        ///MappingFunctionIndex  0=Haldane (independent recombinations), 1=Kossambi
+        /// </summary>
+        public enum MappingIndex
+        {
+            Haldane = 0,
+            Kossambi=1
+        }
+
+        public enum OrganismType
+        {
+            Drosophila=1,
+            PseudoWheat=2,
+            Random=3
+        }
+
+        public enum RecombinationType
+        {
+            Backcross=0,
+            BackcrossWithNoise=1
+        }
+
+        public enum SoftwareStep
+        {
+            None = 0,
+            InputData =1,
+            CalculateData=2,
+            SimulateData=3,
+            ViewResults=4,
+            
+        }
+
+        public enum QTLTaritModels
+        {
+            NoQTL=0,
+            OneQTL=1,
+            OneDominantQTL=2,
+            TwoLinkedQTL=3,
+        }
+
+        public enum TableRowType
+        {
+            InputDataRow=0,
+            GeneticDataRow=1
+        }
+
+        public enum ErrorMessage
+        {
+            MissingPhenotype=0,
+            MissingGenotype=1,
+            BadGeneticMap=2,
+            NoDataLoaded=3
+        }
+
+        public class State
+        {
+            private SoftwareStep currentStep;
+            private SoftwareStep prevoiusStep;
+            private bool goingForward = false;
+         
+            
+            public event EventHandler<StateUpdate> StateChange;
+
+            public void UpdateState(SoftwareStep nextStep, SoftwareStep prevStep,bool goingForward)
+            {
+                StateUpdate args = new StateUpdate();
+                args.PrevoiusStep = prevStep;
+                args.CurrentStep = nextStep;
+                args.GoingForward = goingForward;
+                OnStateChange(args);
+            }
+            public virtual void OnStateChange(StateUpdate e)
+            {
+                StateChange?.Invoke(this, e);
+            }
+
+           
+            public class StateUpdate : EventArgs
+            {
+                public SoftwareStep CurrentStep { get; set; }
+                public SoftwareStep PrevoiusStep { get; set; }
+                public bool GoingForward { get; set; }
+            }  
+        }
+        
+        //public class ListOfValuesWithNA{
+        //    
+        //}
+
+
+    }
+    }
